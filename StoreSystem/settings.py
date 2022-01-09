700ec16cc494882a68a38eee5a75c6409b5de76f
zemin y <958037737@qq.com>
2022-01-09 23:19:53 +0800

    后台管理和view层

--- StoreSystem/settings.py
+++ StoreSystem/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'user.apps.UserConfig',
+    'course.apps.CourseConfig',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'StoreSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'NAME': 'course_store',
+        'USER': 'root',
+        'PASSWORD': 'kaixin2012'
     }
 }
 
